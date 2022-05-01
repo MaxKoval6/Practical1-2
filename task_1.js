a190ef8e6bc41c75e7c822b4c25123729bba1350
MaxKoval6 <mkoval06@gmail.com>
2022-05-01 16:00:27 +0300

    first commit

---
+++ task_1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// task 1
+// const str = "привіт";
+// const num = 123;
+// const flag = true;
+// const txt = "true";
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+
+//task 2
+// const a1 = "Слава Україні!";
+// let b1 = "Героям Слава";
+
+// const a2 = "2022";
+// let b2 = "2023";
+
+// const a3 = "true";
+// let b3 = "false";
+
+// const a4 = "null";
+// let b4 = "null";
+
+// const a5 = "undefined";
+// let b5 = {};
---
+++ task_2.js
@@ -0,0 +1,57 @@
+"use strict";
+//task 1
+// const username = prompt("Введіть ім'я");
+// console.log(username);
+
+//task 2
+// const age1 = 39;
+// const age2 = 18;
+// const agesum = age1 + age2;
+// alert(agesum);
+
+//task3
+//const a = +prompt('first number');
+// const b = +prompt('second number');
+// const sumofnumbers = a*b;
+// console.log(sumofnumbers);
+
+//task4
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+
+//task5
+// let a5 = 5 % 3; //1.3, 1.66, 2
+// let a6 = 3 % 5;
+// let a7 = 5 + "3"; //53
+// let a8 = "5" + 3; //error,null,2
+// let a9 = 75 + "кг"; //75кг
+// alert(a5);
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+
+//task6
+// const width = "10";
+// let height = "23";
+// const s = height * width; //23*10
+// alert(s);
+
+//task7
+// const heightC = 10;
+// const dC = 4;
+// const v = Math.PI * (dC / 2) ** 2 * heightC;
+// //Math.PI * Math.pow(dC / 2, 2) * heightC;
+// console.log(v)
+
+//task8
+// const n = 3;
+// const m = 4;
+// const k = Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
+// alert(k);
