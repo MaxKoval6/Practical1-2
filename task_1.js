6d7a550df50e30a9ba076899f41f777c5c6a96c6
MaxKoval6 <mkoval06@gmail.com>
2022-05-08 13:08:30 +0300

    ready

--- task_1.js
+++ task_1.js
@@ -11,17 +11,17 @@
 // console.log(typeof txt);
 
 //task 2
-// const a1 = "Слава Україні!";
-// let b1 = "Героям Слава";
+// const a1 = Слава Україні!;
+// let b1 = Героям Слава;
 
-// const a2 = "2022";
-// let b2 = "2023";
+// const a2 = 2022;
+// let b2 = 2023;
 
-// const a3 = "true";
-// let b3 = "false";
+// const a3 = true;
+// let b3 = false;
 
-// const a4 = "null";
-// let b4 = "null";
+// const a4 = null;
+// let b4 = null;
 
-// const a5 = "undefined";
+// const a5 = undefined;
 // let b5 = {};
